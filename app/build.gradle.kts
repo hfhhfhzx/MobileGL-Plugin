ae0caecfb218e008d90d426a1ca2b46c6abebbb8
hfhhfhzx <hfhhfhzx@gmail.com>
2026-03-28 14:44:13 +0800

    新的签名配置

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,6 @@
+import com.android.build.api.dsl.ApplicationBuildType
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
 }
@@ -5,6 +8,12 @@ plugins {
 val gitCommitCount: Int by lazy { runGitCommand("rev-list", "--count", "HEAD")?.toIntOrNull() ?: 0 }
 
 val gitVersionCode: Int by lazy { 1198 + gitCommitCount }
+
+val properties: Properties? = loadPropertiesFromFile("signing.properties")
+    fun getString(propertyName: String, environmentName: String, prompt: String): String =
+        properties?.getProperty(propertyName)
+            ?: System.getenv(environmentName)
+            ?: System.console()?.readLine("\n$prompt: ").orEmpty()
 
 fun runGitCommand(vararg args: String): String? = runCatching {
     ProcessBuilder(listOf("git") + args)
@@ -15,6 +24,11 @@ fun runGitCommand(vararg args: String): String? = runCatching {
             if (process.waitFor() == 0 && output.isNotBlank()) output else null
         }
 }.getOrNull()
+
+fun loadPropertiesFromFile(fileName: String): Properties? =
+    rootProject.file(fileName).takeIf { it.exists() }?.let { file ->
+        Properties().apply { load(file.inputStream()) }
+    }
 
 android {
     namespace = "com.mio.plugin.renderer"
@@ -37,11 +51,17 @@ android {
     }
     
     signingConfigs {
-        create("release") {
-            storeFile = file("../keystore")
-            storePassword = System.getenv("SIGNING_STORE_PASSWORD") ?: project.findProperty("SIGNING_STORE_PASSWORD") as String?
-            keyAlias = System.getenv("SIGNING_KEY_ALIAS") ?: project.findProperty("SIGNING_KEY_ALIAS") as String?
-            keyPassword = System.getenv("SIGNING_KEY_PASSWORD") ?: project.findProperty("SIGNING_KEY_PASSWORD") as String?
+        create("hasProperties") {
+            if (properties != null) {
+                storeFile = file(getString("storeFile", "STORE_FILE", "Store file"))
+                storePassword = getString("storePassword", "STORE_PASSWORD", "Store password")
+                keyAlias = getString("keyAlias", "KEY_ALIAS", "Key alias")
+                keyPassword = getString("keyPassword", "KEY_PASSWORD", "Key password")
+            }
+            enableV1Signing = false
+            enableV2Signing = true
+            enableV3Signing = false
+            enableV4Signing = false
         }
     }
 
@@ -56,32 +76,43 @@ android {
     }
     
     buildTypes {
+        val configSigning: ApplicationBuildType.() -> Unit = {
+            val signingConfigName = if (properties != null) "hasProperties" else "debug"
+            signingConfig = signingConfigs.findByName(signingConfigName)
+        }
+        
         release {
-            signingConfig = signingConfigs.getByName("release")
+            configSigning()
             isMinifyEnabled = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
+        
+        debug {
+            configSigning()
+            isMinifyEnabled = false
+        }
+        
         configureEach {
-            //应用名
+            // 应用名
             //app name
             resValue("string","app_name","MobileGL")
-            //包名后缀
+            // 包名后缀
             //package name Suffix
             applicationIdSuffix = ".mobilegl"
 
-            //渲染器在启动器内显示的名称
+            // 渲染器在启动器内显示的名称
             //The name displayed by the renderer in the launcher
             manifestPlaceholders["des"] = "MobileGL"
-            //渲染器的具体定义 格式为 名称:渲染器库名:EGL库名 例如 LTW:libltw.so:libltw.so
-            //The specific definition format of a renderer is ${name}:${renderer library name}:${EGL library name}, for example:   LTW:libltw.so:libltw.so
+            // 渲染器的具体定义 格式为 名称:渲染器库名:EGL库名 例如 LTW:libltw.so:libltw.so
+            // The specific definition format of a renderer is ${name}:${renderer library name}:${EGL library name}, for example:   LTW:libltw.so:libltw.so
             manifestPlaceholders["renderer"] = "MobileGL:libMobileGL.so:libMobileGL.so"
 
-            //特殊Env
-            //Special Env
-            //DLOPEN=libxxx.so 用于加载额外库文件
-            //DLOPEN=libxxx.so used to load external library
+            // 特殊Env
+            // Special Env
+            // DLOPEN=libxxx.so 用于加载额外库文件
+            // DLOPEN=libxxx.so used to load external library
             //如果有多个库,可以使用","隔开,例如  DLOPEN=libxxx.so,libyyy.so
-            //If there are multiple libraries, you can use "," to separate them, for example  DLOPEN=libxxx.so,libyyy.so
+            // If there are multiple libraries, you can use "," to separate them, for example  DLOPEN=libxxx.so,libyyy.so
             manifestPlaceholders["boatEnv"] = mutableMapOf<String,String>().apply {
                 put("LIBGL_ES", "3")
             }.run {
@@ -105,12 +136,14 @@ android {
                 env.dropLast(1)
             }
 
-            //最小支持的MC版本
-            //The minimum supported MC version
+             // 为空则不限制 
+             // No restriction if empty
+            // 最小支持的MC版本
+            // The minimum supported MC version
             manifestPlaceholders["minMCVer"] = "1.17"
-            //最大支持的MC版本
-            //The maximum supported MC version
-            manifestPlaceholders["maxMCVer"] = "" //为空则不限制 No restriction if empty
+            // 最大支持的MC版本
+            // The maximum supported MC version
+            manifestPlaceholders["maxMCVer"] = ""
         }
     }
 }
