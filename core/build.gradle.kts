06ad2441fea84e811d5b330a017c015b4497e67f
hfhhfhzx <hfhhfhzx@gmail.com>
2026-07-20 14:11:44 +0800

    try 1

---
+++ core/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    alias(libs.plugins.lsplugin.cmaker)
+}
+
+cmaker {
+    default {
+        // 指定编译的目标 CPU 架构
+        // 可用值：arm64-v8a, armeabi-v7a, x86, x86_64, riscv64
+        abiFilters("arm64-v8a")
+    }
+}
+
+android {
+    namespace = "top.mobilegl.mobilegl"
+    compileSdk = 37
+
+    defaultConfig {
+        minSdk = 21
+    }
+
+    buildTypes {
+        release{
+            optimization {
+                enable = true
+            }
+        }
+    }
+
+    externalNativeBuild {
+        cmake {
+            path = file("MobileGL/CMakeLists.txt")
+        }
+    }
+
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,5 +20,5 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "FCLRendererPlugin"
-include(":app")
+include(":app", ":core")
  
